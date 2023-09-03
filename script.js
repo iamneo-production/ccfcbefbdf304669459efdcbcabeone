20fab42d67b4f92fa6c3beeac5d20290cd89c89e
Iamneo <techadmin@iamneo.ai>
2023-09-03 05:07:05 +0000

    Backup Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const board = document.getElementById('board');
+const cells = document.querySelectorAll('[data-cell]');
+const message = document.getElementById('message');
+const restartButton = document.getElementById('restart-button');
+
+let currentPlayer ='X';
