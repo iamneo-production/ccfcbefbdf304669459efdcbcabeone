8d2c79c9001bec403e4d69fbce55dfcb7325b9d1
Iamneo <techadmin@iamneo.ai>
2023-09-03 05:17:04 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -21,6 +21,55 @@ function checkWin() {
 
     for(const pattern of winPatterns){
         const [a,b,c]=pattern;
-        
+        if(gameBoard[a] &&gameBoard[a] === gameBoard[b] && gameBoard[a] ===gameBoard[c]){
+            gameActive = false;
+            highlightWinningCells(pattern);
+            message.innerText = '${currentPlayer} wins!';
+            break;
+
+        }
+    }
+
+    if(!gameBoard.includes('') && gameActive){
+        gameActive = false;
+        message.innerText = "It's a draw!";
     }
 }
+function highlightWinningCells(pattern) {
+    for (const index of pattern) {
+        cells[index].style.backgroundColor = '#4caf50';
+        cells[index].style.color = '#fff';
+    }
+}
+
+function handleClick(event) {
+    const cell = event.target;
+    const cellIndex = [...cells].indexOf(cell);
+
+    if (gameBoard[cellIndex] === '' && gameActive) {
+        cell.innerText = currentPlayer;
+        gameBoard[cellIndex] = currentPlayer;
+        checkWin();
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        message.innerText = `Player ${currentPlayer}'s Turn`;
+    }
+}
+
+function restartGame() {
+    currentPlayer = 'X';
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    gameActive = true;
+    message.innerText = `Player ${currentPlayer}'s Turn`;
+
+    cells.forEach((cell) => {
+        cell.innerText = '';
+        cell.style.backgroundColor = '#ccc';
+        cell.style.color = '#000';
+    });
+}
+
+cells.forEach((cell) => {
+    cell.addEventListener('click', handleClick);
+});
+
+restartButton.addEventListener('click', restartGame);
