239ea28f6faee385b0c94b89f35b2ea344e8d65a
Iamneo <techadmin@iamneo.ai>
2023-09-03 05:12:04 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -3,4 +3,24 @@ const cells = document.querySelectorAll('[data-cell]');
 const message = document.getElementById('message');
 const restartButton = document.getElementById('restart-button');
 
-let currentPlayer ='X';
+let currentPlayer = 'X';
+let gameBoard =['','','','','','','','',''];
+let gameActive =true;
+
+function checkWin() {
+    const winPatterns = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    for(const pattern of winPatterns){
+        const [a,b,c]=pattern;
+        
+    }
+}
