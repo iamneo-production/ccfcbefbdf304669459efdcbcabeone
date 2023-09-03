a60cb96ea6e87470f32ecfa8dfe3dc3ed9f59b64
Iamneo <techadmin@iamneo.ai>
2023-09-03 05:25:39 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -4,37 +4,41 @@ const message = document.getElementById('message');
 const restartButton = document.getElementById('restart-button');
 
 let currentPlayer = 'X';
-let gameBoard =['','','','','','','','',''];
-let gameActive =true;
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
 
 function checkWin() {
     const winPatterns = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ];
 
-    for(const pattern of winPatterns){
-        const [a,b,c]=pattern;
-        if(gameBoard[a] &&gameBoard[a] === gameBoard[b] && gameBoard[a] ===gameBoard[c]){
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
             gameActive = false;
             highlightWinningCells(pattern);
-            message.innerText = '${currentPlayer} wins!';
+            setTimeout(() => {
+                message.innerText = `${currentPlayer} wins!`;
+            }, 500);
             break;
-
         }
     }
 
-    if(!gameBoard.includes('') && gameActive){
+    if (!gameBoard.includes('') && gameActive) {
         gameActive = false;
-        message.innerText = "It's a draw!";
+        setTimeout(() => {
+            message.innerText = "It's a draw!";
+        }, 500);
     }
 }
+
 function highlightWinningCells(pattern) {
     for (const index of pattern) {
         cells[index].style.backgroundColor = '#4caf50';
@@ -48,6 +52,7 @@ function handleClick(event) {
 
     if (gameBoard[cellIndex] === '' && gameActive) {
         cell.innerText = currentPlayer;
+        cell.style.transform = 'scale(1.1)';
         gameBoard[cellIndex] = currentPlayer;
         checkWin();
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
@@ -63,8 +68,9 @@ function restartGame() {
 
     cells.forEach((cell) => {
         cell.innerText = '';
-        cell.style.backgroundColor = '#ccc';
+        cell.style.backgroundColor = '#ddd';
         cell.style.color = '#000';
+        cell.style.transform = 'scale(1)';
     });
 }
 
